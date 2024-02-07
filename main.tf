4a31d0ee372995ca0c7da8dcd8a70363d4db7bdb
ZeeshanQ <zeeshan.quraishy@fremtind.no>
2024-02-07 13:48:17 +0100

    tenant

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ provider "azurerm" {
     subscription_id = var.ARM_SUBSCRIPTION_ID
     client_id = var.ARM_CLIENT_ID
     client_secret = var.ARM_SECRET
+    tenant_id = var.ARM_TENANT_ID
     features {
       
     }
@@ -38,5 +39,8 @@ variable "ARM_SUBSCRIPTION_ID" {
   
 }
 variable "ARM_CLIENT_ID" {
+  
+}
+variable "ARM_TENANT_ID" {
   
 }
