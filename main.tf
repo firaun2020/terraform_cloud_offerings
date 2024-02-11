d2d9fccc11b1169dfb0dd3b0f14cf97c136dbbc9
ZQ-Predator <zeeshan.quraishy@outlook.com>
2024-02-11 14:49:38 +0100

    updated fmt

--- main.tf
+++ main.tf
@@ -1,43 +1,43 @@
 terraform {
   cloud {
     organization = "firaun2020"
-    token = var.ARM_SECRET
+    token        = var.ARM_SECRET
     workspaces {
       name = "terraform_cloud_offerings"
-      
+
     }
-  }  
+  }
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.90.0"
     }
   }
 }
 
 provider "azurerm" {
-    subscription_id = var.ARM_SUBSCRIPTION_ID
-    client_id = var.ARM_CLIENT_ID
-    client_secret = var.ARM_SECRET
-    tenant_id = var.ARM_TENANT_ID
-    skip_provider_registration = true
-    features {
-      
-    }
+  subscription_id            = var.ARM_SUBSCRIPTION_ID
+  client_id                  = var.ARM_CLIENT_ID
+  client_secret              = var.ARM_SECRET
+  tenant_id                  = var.ARM_TENANT_ID
+  skip_provider_registration = true
+  features {
+
+  }
 }
 
 provider "azurerm" {
-  alias = "radio"
-    subscription_id = var.ARM_SUBSCRIPTION_ID
-    client_id = var.ARM_CLIENT_ID
-    client_secret = var.ARM_SECRET
-    tenant_id = var.ARM_TENANT_ID
-    skip_provider_registration = true
-  
-    features {
-      
-      
-    }
+  alias                      = "radio"
+  subscription_id            = var.ARM_SUBSCRIPTION_ID
+  client_id                  = var.ARM_CLIENT_ID
+  client_secret              = var.ARM_SECRET
+  tenant_id                  = var.ARM_TENANT_ID
+  skip_provider_registration = true
+
+  features {
+
+
+  }
 }
 
 
@@ -45,15 +45,15 @@ provider "azurerm" {
 #     count = 5
 #     name = "rg-${count.index}"
 #     location = "West Europe"
-    
-  
+
+
 # }
 
 # resource "azurerm_resource_group" "second-tier" {
 #     count = length(azurerm_resource_group.rg-1)
 #     name = "${azurerm_resource_group.rg-1[count.index].name}-${count.index}"
 #     location = azurerm_resource_group.rg-1[count.index].location
-  
+
 # }
 
 
--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "ARM_SECRET" {
- 
+
 }
 variable "ARM_SUBSCRIPTION_ID" {
-  
+
 }
 variable "ARM_CLIENT_ID" {
-  
+
 }
 variable "ARM_TENANT_ID" {
-  
+
 }
 
 variable "mapset" {
-    type = map(string)
-    default = {
-        "Ik" : "Wil-win",
-        "NS" : "Will-lose",
-        "BB" : "Will-lose"
-    }
-  
+  type = map(string)
+  default = {
+    "Ik" : "Wil-win",
+    "NS" : "Will-lose",
+    "BB" : "Will-lose"
+  }
+
 }
